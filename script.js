59d3dd832d806875c8eeaece3a45cf720d50787d
Caleb <cmogame@outlook.com>
2022-04-14 13:02:22 -0700

    Add interactive RPS buttons

---
+++ script.js
@@ -0,0 +1,67 @@
+let playerScore = 0;
+let computerScore = 0;  
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const results = document.querySelector('#results');
+
+
+rock.addEventListener('click', () => playRound('rock'));
+paper.addEventListener('click', () => playRound('paper'));
+scissors.addEventListener('click', () => playRound('scissors'));   
+  
+/*alert("The side with the most rounds won in 5 games is declared the winner.")
+game();
+finalAnnouncement();*/
+
+
+function computerPlay() {
+  // Assign the three choices to an array
+  const computerChoices = ["rock", "paper", "scissors"];
+  // Randomly pick one of the choices with equal chances
+  return computerChoices[Math.floor(Math.random()*computerChoices.length)];
+}
+    
+
+function playRound(selection) {
+  let playerSelection = selection;
+  console.log(playerSelection);
+  let computerSelect = computerPlay();
+  console.log(computerSelect);
+  // Convert playerInput to lowercase to make it case insensitive
+  let playerSelect = playerSelection.toLowerCase();
+  if (playerSelect === computerSelect) {
+      let roundResult = `${playerSelect} versus ${computerSelect}! It's a tie!`;
+      console.log(roundResult);
+  } else if ( // If player wins
+      (playerSelect === "rock") && (computerSelect === "scissors") ||
+      (playerSelect === "paper") && (computerSelect === "rock") ||
+      (playerSelect === "scissors") && (computerSelect === "paper")
+  ) {
+      let roundResult = `${playerSelect} versus ${computerSelect}! You won!`;
+      console.log(roundResult);
+      playerScore ++;
+  } else {
+      let roundResult = `${playerSelect} versus ${computerSelect}! You lost!`;
+      console.log(roundResult);
+      computerScore ++;
+  }
+}
+  
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+    console.log(`The score is ${playerScore}-${computerScore}`);
+  }
+}
+
+function finalAnnouncement() {
+  if (playerScore === computerScore) {
+    alert(`The final score is ${playerScore}-${computerScore}. It's a draw!`);
+  } else if (playerScore < computerScore) {
+    alert(`The final score is ${playerScore}-${computerScore}. You are a loser!`);
+  } else if (playerScore > computerScore) {
+    alert(`The final score is ${playerScore}-${computerScore}. Winner winner chicken dinner!`);
+  }
+}
